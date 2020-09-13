f1db699d7f3208aa921fdaad3976dc78114055a6
Gustavo Nogales <gustavo.nogales@hotmail.com>
2020-09-13 16:56:34 -0300

    Adding TSYRINGE for dependency injection

--- dist/app/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("reflect-metadata");
-require("@app/container");
-require("@config/expressConfig");
--- dist/web/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var tsyringe_1 = require("tsyringe");
-var userRouter = express_1.Router();
-var userController = tsyringe_1.container.resolve('userController');
-userRouter.get('/', userController.findAll);
-exports.default = userRouter;
